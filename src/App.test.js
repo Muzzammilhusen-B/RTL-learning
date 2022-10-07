1f4ef9c140798e159bd693e500b64bb6509dfa2a
Muzzammilhusen-B <muzammilhusen.b@applocum.org>
2022-10-07 12:14:49 +0530

    Used getbyrole method of RTL

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import {render, screen} from "@testing-library/react";
 import App from "./App";
 
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText("Learn React");
+//   expect(linkElement).toBeInTheDocument();
+// });
+// test("1renders learn react link", () => {
+//   throw new Error("tests failed!!");
+// });
+
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText("Learn React");
+  const linkElement = screen.getByRole("link", {name: /learn react/i});
   expect(linkElement).toBeInTheDocument();
 });
