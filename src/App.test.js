f740bdcbbf2f1515758eed80df2af5cd1fe5a0cc
Muzzammilhusen-B <muzammilhusen.b@applocum.org>
2022-10-10 09:20:52 +0530

    Test and code checkbox functionality

--- src/App.test.js
+++ src/App.test.js
@@ -21,3 +21,28 @@ test("button turns blue when clicked", () => {
 
   expect(colorButton.textContent).toBe("Change to red");
 });
+
+test("initial condition", () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole("button", {name: "Change to blue"});
+
+  expect(colorButton).toBeEnabled();
+
+  const checkBox = screen.getByRole("checkbox");
+
+  expect(checkBox).not.toBeChecked();
+});
+
+test("Chekcbox disables button in first click and enables on second click", () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole("button");
+  const checkBox = screen.getByRole("checkbox");
+
+  fireEvent.click(checkBox);
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkBox);
+  expect(colorButton).toBeEnabled();
+});
